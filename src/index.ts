ae620652a6952c1ee6e89dbda9750d029ae1f726
hibara428 <hibara428@gmail.com>
2024-04-23 07:11:02 +0900

    Implement base endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,76 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-}
-
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
+type Bindings = {
+    article: KVNamespace;
 };
+
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import { etag } from 'hono/etag';
+
+export type Post = {
+    datetime: number;
+    text: string;
+    lat?: string;
+    lon?: string;
+    location?: string;
+    images?: string[];
+};
+export type Param = {
+    text: string;
+    lat?: string;
+    lon?: string;
+    location?: string;
+    images?: string[];
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+app.use('*', cors(), etag());
+
+app.get('/', async (context) => {
+    const keys = await context.env.article.list();
+    return context.json(
+        {
+            message: 'ok',
+            data: keys.keys,
+        },
+        200
+    );
+});
+
+app.post('/post', async (context) => {
+    try {
+        const param = await context.req.json<Param>();
+        if (!param.text) throw new Error('text is required');
+        const datetime = Math.floor(Date.now() / 1000);
+        const post: Post = {
+            datetime: datetime,
+            text: param.text,
+            lat: param.lat,
+            lon: param.lon,
+            location: param.location,
+            images: param.images,
+        };
+        await context.env.article.put(datetime.toString(), JSON.stringify(post));
+        return context.json({ message: 'ok', id: datetime }, 200);
+    } catch (e: any) {
+        console.log(e.message);
+        return context.json({ error: 'invalid request' }, 400);
+    }
+});
+
+app.get('/post/:id', async (context) => {
+    const id = context.req.param('id');
+    const post = await context.env.article.get(id);
+    if (!post) return context.json({ error: 'not found' }, 404);
+
+    return context.json({ message: 'ok', data: JSON.parse(post) as Post }, 200);
+});
+
+app.delete('/post/:id', async (context) => {
+    const id = context.req.param('id');
+    const post = await context.env.article.get(id);
+    if (!post) return context.json({ error: 'not found' }, 404);
+    await context.env.article.delete(id);
+    return context.json({ message: 'ok' }, 200);
+});
+
+export default app;

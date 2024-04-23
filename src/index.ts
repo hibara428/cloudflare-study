81f64c196b6ff50897157022a781ab54ad6ffc8e
hibara428 <hibara428@gmail.com>
2024-04-24 08:10:16 +0900

    Change to use D1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 type Bindings = {
+    DB: D1Database;
     article: KVNamespace;
 };
 
@@ -21,16 +22,29 @@ export type Param = {
     location?: string;
     images?: string[];
 };
+export type DetailDB = {
+    id: number;
+    text: string;
+    lat?: string;
+    lon?: string;
+    location?: string;
+};
+export type ImageDB = {
+    detail_id: number;
+    image: string;
+};
 
 const app = new Hono<{ Bindings: Bindings }>();
 app.use('*', cors(), etag());
 
 app.get('/', async (context) => {
-    const keys = await context.env.article.list();
+    const { results } = await context.env.DB.prepare('SELECT id FROM Details ORDER BY id desc').all<DetailDB>();
+    const id_list = results?.map((data: DetailDB) => data.id);
+
     return context.json(
         {
             message: 'ok',
-            data: keys.keys,
+            data: id_list,
         },
         200
     );
@@ -41,15 +55,19 @@ app.post('/post', async (context) => {
         const param = await context.req.json<Param>();
         if (!param.text) throw new Error('text is required');
         const datetime = Math.floor(Date.now() / 1000);
-        const post: Post = {
-            datetime: datetime,
-            text: param.text,
-            lat: param.lat,
-            lon: param.lon,
-            location: param.location,
-            images: param.images,
-        };
-        await context.env.article.put(datetime.toString(), JSON.stringify(post));
+
+        const result = await context.env.DB.prepare('INSERT INTO Details (id, text, lat, lon, location) VALUES (?, ?, ?, ?, ?)')
+            .bind(datetime.toString(), param.text, param.lat, param.lon, param.location)
+            .run();
+        if (!result.success) {
+            return context.json({ message: 'register failed' }, 500);
+        }
+
+        if (param.images && param.images.length != 0) {
+            const stmt = context.env.DB.prepare('INSERT INTO Images (detail_id, image) VALUES (?, ?)');
+            await context.env.DB.batch(param.images.map((value: string) => stmt.bind(datetime.toString(), value)));
+        }
+
         return context.json({ message: 'ok', id: datetime }, 200);
     } catch (e: any) {
         console.log(e.message);
@@ -59,18 +77,53 @@ app.post('/post', async (context) => {
 
 app.get('/post/:id', async (context) => {
     const id = context.req.param('id');
-    const post = await context.env.article.get(id);
-    if (!post) return context.json({ error: 'not found' }, 404);
+    const post = await context.env.DB.prepare('SELECT * FROM Details WHERE id = ?').bind(id).first<DetailDB>();
 
-    return context.json({ message: 'ok', data: JSON.parse(post) as Post }, 200);
+    if (typeof post === 'undefined' || post === null) {
+        return context.json({ error: 'not found' }, 404);
+    }
+    let images: string[] = [];
+
+    const image_list = await context.env.DB.prepare('SELECT * FROM Images WHERE detail_id = ?').bind(id).all<ImageDB>();
+    if (image_list && image_list.results) {
+        images = image_list.results.map((value: ImageDB) => value.image);
+    }
+
+    const result_post: Post = {
+        datetime: Number(id),
+        text: post.text,
+        lat: post.lat,
+        lon: post.lon,
+        location: post.location,
+        images: images,
+    };
+
+    return context.json({ message: 'ok', data: result_post as Post }, 200);
 });
 
 app.delete('/post/:id', async (context) => {
     const id = context.req.param('id');
-    const post = await context.env.article.get(id);
-    if (!post) return context.json({ error: 'not found' }, 404);
-    await context.env.article.delete(id);
+    const post = await context.env.DB.prepare('SELECT * FROM Details WHERE id = ?').bind(id).first<DetailDB>();
+    if (typeof post === 'undefined' || post === null) {
+        return context.json({ error: 'not found' }, 404);
+    }
+
+    const detail_result = await context.env.DB.prepare('DELETE FROM Details WHERE id = ?').bind(id).run();
+
+    if (!detail_result.success) {
+        return context.json({ message: 'delete failed' }, 500);
+    }
+    const image_result = await context.env.DB.prepare('DELETE FROM Images WHERE detail_id = ?').bind(id).run();
+
+    if (!image_result.success) {
+        return context.json({ message: 'delete failed' }, 500);
+    }
+
     return context.json({ message: 'ok' }, 200);
+});
+
+app.get('/nearby/:lat/:lon', async (context) => {
+    return context.json({ message: 'ok', locations: ['l1', 'l2', 'l3'] }, 200);
 });
 
 export default app;
